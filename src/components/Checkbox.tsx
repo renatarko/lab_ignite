ce361e31a20824d8ff6f4c436a039b587fd0df14
Renata Karonlina <renatakarolinarko@gmail.com>
2022-10-13 13:30:23 -0400

    first commit

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,9 @@
+import { Slot } from "@radix-ui/react-slot";
+import { clsx } from "clsx";
+import { ReactNode } from "react";
+
+export interface CheckboxProps {}
+
+export function Checkbox() {
+  return;
+}
